db3d00d0ef611102dbb91cb6703c5b6e01ae68bd
John Lawrence <thegordo.john@gmail.com>
2011-02-26 22:18:39 -0700

    Will now create a user if the login is valid for LDAP but employee not in the database.

--- src/main/java/timeSheet/Login.java
+++ src/main/java/timeSheet/Login.java
@@ -1,10 +1,14 @@
 package timeSheet;
 
 import timeSheet.database.entity.Employee;
+import timeSheet.database.entity.GroupRole;
 import timeSheet.database.manager.EmployeeManager;
+import timeSheet.database.manager.GroupManager;
 import timeSheet.database.manager.SettingsManager;
 import timeSheet.util.TimeSheetException;
 import timeSheet.util.ldap.LDAPAuthenticate;
+
+import java.util.Date;
 
 /**
  * User: John Lawrence
@@ -16,9 +20,11 @@ public class Login {
     private String password;
     private LDAPAuthenticate ldap;
     private String failureMessage;
+    private String name;
 
     public Login(String name, String password) {
         this.password = password;
+        this.name = name;
         EmployeeManager manager = new EmployeeManager();
         employee = manager.getEmployee(name);
         SettingsManager settings = new SettingsManager();
@@ -31,12 +37,32 @@ public class Login {
 
     public boolean checkLDAPLogin(String password) {
         try {
-            ldap.login(employee.getUserName(), password);
+            ldap.login(name, password);
+            if (employee == null) {
+                createEmployee(name, password);
+            }
             return true;
         } catch (TimeSheetException e) {
             failureMessage = e.getMessage();
             return false;
         }
+    }
+
+    private void createEmployee(String name, String password) {
+        Employee employee = new Employee();
+        employee.setHireDate(new Date());
+        employee.setFullTimeDate(null);
+        employee.setActiveFlag(true);
+        GroupManager manager = new GroupManager();
+        employee.setGroup(manager.getGroup(1));
+        employee.setEmailAddress("");
+        employee.setFileNumber("");
+        employee.setName(name);
+        employee.setUserName(name);
+        employee.setRole(GroupRole.Employee);
+        employee.setSalary(false);
+        employee.setPtoAllowed(false);
+        employee.setPassword(password);
     }
 
     public Employee getEmployee() {
