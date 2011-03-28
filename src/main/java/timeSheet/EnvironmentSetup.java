2601f93ea5bb50c1cfc0b2d970cb759709aa164f
John Lawrence <thegordo.john@gmail.com>
2011-03-28 06:40:31 -0600

    Added a listener to make the H2 Server come up and down with tomcat.

---
+++ src/main/java/timeSheet/EnvironmentSetup.java
@@ -0,0 +1,41 @@
+package timeSheet;
+
+import org.h2.tools.Server;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * User: John Lawrence
+ * Date: 3/27/11
+ * Time: 8:17 AM
+ */
+public class EnvironmentSetup implements ServletContextListener {
+    private Logger logger = Logger.getLogger(EnvironmentSetup.class.getName());
+
+    @Override
+    public void contextInitialized(ServletContextEvent servletContextEvent) {
+        try {
+            logger.info("Starting the H2 server!");
+            Server.createTcpServer().start();
+            logger.info("H2 server started.");
+        } catch (SQLException e) {
+            logger.log(Level.SEVERE, e.getMessage(), e);
+            System.exit(300);
+        }
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent servletContextEvent) {
+        logger.info("Attempting to stop the H2 server!");
+        try {
+            Server.shutdownTcpServer("tcp://localhost:9092", "", true, true);
+        } catch (SQLException e) {
+            logger.log(Level.SEVERE, e.getMessage(), e);
+        }
+        logger.info("H2 server shutdown.");
+    }
+}
