c52b51071c2363c61c7b04a12f383ec2d4c23cad
John Lawrence <thegordo.john@gmail.com>
2011-04-11 23:24:41 -0600

    Got a test fixed.

--- src/test/java/timeSheet/database/DBTypeTest.java
+++ src/test/java/timeSheet/database/DBTypeTest.java
@@ -1,6 +1,13 @@
 package timeSheet.database;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+import timeSheet.util.PaySystemProperties;
+import timeSheet.util.PropertyName;
 
 import static junit.framework.Assert.assertTrue;
 
@@ -9,9 +16,13 @@ import static junit.framework.Assert.assertTrue;
  * Date: 3/13/11
  * Time: 8:39 PM
  */
+@RunWith(PowerMockRunner.class)
+@PrepareForTest(PaySystemProperties.class)
 public class DBTypeTest {
     @Test
     public void testGetOptions() throws Exception {
+        PowerMockito.mockStatic(PaySystemProperties.class);
+        Mockito.when(PaySystemProperties.getProperty(PropertyName.DB_TYPE)).thenReturn("H2");
         String options = DBType.getSelection();
         for (DBType type : DBType.values()) {
             assertTrue(options.contains(type.getDisplayName()));
